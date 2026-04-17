c57e806c1d536b859432cb75ea084b08be077364
supermohamed55555-hash <supermohamed55555@gmail.com>
2026-04-17 17:56:46 +0200

    Deployment: Support dynamic PORT environment variable

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
